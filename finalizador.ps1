b2286a0ebfbff0c43b2a8aac0e49e042ad2f05b8
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-25 12:21:32 -0300

    Adicionando arquivos..

---
+++ finalizador.ps1
@@ -0,0 +1,13 @@
+# Remover todos os perfis do Chrome
+$userName = $env:USERNAME
+$chromeUserDataPath = "C:\Users\$userName\AppData\Local\Google\Chrome\User Data"
+
+# Obter lista de diretórios de perfil
+$profileDirectories = Get-ChildItem -Path $chromeUserDataPath 
+
+# Remover cada diretório de perfil
+foreach ($profileDirectory in $profileDirectories) {
+    Remove-Item -Path $profileDirectory.FullName -Force -Recurse
+}
+
+Remove-Item -Path "C:\Users\$userName\Desktop\*.lnk" -Force
