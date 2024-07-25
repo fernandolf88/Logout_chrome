fd07349b3e0ce9cefa858e41244efc678ee6d3ba
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-25 15:47:45 -0300

    Atualizando...

--- install_script.bat
+++ install_script.bat
@@ -4,7 +4,7 @@ set url=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/finali
 mkdir C:\script\
 set output = C:\script\logout.ps1
 curl %url% -o c:\script\logout.ps1
-schtasks /create /tn "Logout Chrome" /tr "powershell.exe -ExecutionPolicy Bypass -File C:\script\logout.ps1" /sc onstart
+schtasks /create /tn "Logout Chrome" /tr "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File C:\script\logout.ps1" /sc onstart
 attrib +h c:\script
 schtasks /run /tn "Logout Chrome"
 msg * "Script instalado com sucesso!"
