7b8bab539a299b86f7dc9aaf0fefe2caaaf58299
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-26 14:24:07 -0300

    Atualizando

--- finalizador.ps1
+++ finalizador.ps1
@@ -13,16 +13,3 @@ foreach ($profileDirectory in $profileDirectories) {
 Remove-Item -Path "C:\Users\$userName\Desktop\*.lnk" -Force
 
 Expand-Archive -Path "C:\script\User.zip" -DestinationPath "C:\Users\$userName\AppData\Local\Google\Chrome\" -Force
-
-[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-
-$form = New-Object System.Windows.Forms.Form
-$form.Text = "Tarefa Finalizada"
-$form.Size = New-Object System.Drawing.Size(500, 100)
-
-$label = New-Object System.Windows.Forms.Label
-$label.Text = "A tarefa foi finalizada 
-                com sucesso."
-$label.Location = New-Object System.Drawing.Point(30, 30)
-$form.Controls.Add($label)
-$form.ShowDialog()
