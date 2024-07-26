a9337c85f468fc145788b77806f98709db2791b6
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-26 12:59:02 -0300

    aaaaaaaaa

--- finalizador.ps1
+++ finalizador.ps1
@@ -5,8 +5,6 @@ $chromeUserDataPath = "C:\Users\$userName\AppData\Local\Google\Chrome\User Data"
 # Obter lista de diretórios de perfil
 $profileDirectories = Get-ChildItem -Path $chromeUserDataPath 
 
-Get-Process -Verbose:$false
-
 # Remover cada diretório de perfil
 foreach ($profileDirectory in $profileDirectories) {
     Remove-Item -Path $profileDirectory.FullName -Force -Recurse
@@ -19,11 +17,11 @@ Expand-Archive -Path "C:\script\User.zip" -DestinationPath "C:\Users\$userName\A
 [System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
 
 $form = New-Object System.Windows.Forms.Form
-$form.Text = "Tarefa Concluída"
+$form.Text = "Tarefa Finalizada"
 $form.Size = New-Object System.Drawing.Size(300, 100)
 
 $label = New-Object System.Windows.Forms.Label
-$label.Text = "A tarefa foi concluída com sucesso."
+$label.Text = "A tarefa foi finalizada com sucesso."
 $label.Location = New-Object System.Drawing.Point(10, 10)
 $form.Controls.Add($label)
 
