7315a6239f15642a6024f3c85c1cce3dfdd3a35f
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-26 12:48:18 -0300

    ufa

--- finalizador.ps1
+++ finalizador.ps1
@@ -16,4 +16,16 @@ Remove-Item -Path "C:\Users\$userName\Desktop\*.lnk" -Force
 
 Expand-Archive -Path "C:\script\User.zip" -DestinationPath "C:\Users\$userName\AppData\Local\Google\Chrome\" -Force
 
-Write-Host "Script executado com exito."
+[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
+
+$form = New-Object System.Windows.Forms.Form
+$form.Text   
+ = "Tarefa Concluída"
+$form.Size = New-Object System.Drawing.Size(300, 100)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Text = "A tarefa foi concluída com sucesso."
+$label.Location = New-Object System.Drawing.Point(10, 10)
+$form.Controls.Add($label)
+
+$form.ShowDialog()
--- install_script.bat
+++ install_script.bat
@@ -1,4 +1,4 @@
-@ECHO ON
+@ECHO OFF
 
 set url1=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/finalizador.ps1
 set url2=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/executar.bat
@@ -7,7 +7,7 @@ mkdir C:\script\
 curl %url1% -o c:\script\logout.ps1
 curl %url2% -o c:\script\executar.bat
 curl %url3% -o c:\script\User.zip
-schtasks /create /tn "Logout Chrome" /tr "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File C:\script\logout.ps1" /sc onlogon
+schtasks /create /tn "Logout Chrome" /tr "powershell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -WindowStyle Hidden -File C:\script\logout.ps1" /sc onlogon
 schtasks /create /tn "Run Script" /tr "C:\executar.bat" /sc onlogon
 attrib +h c:\script
 schtasks /run /tn "Logout Chrome"
