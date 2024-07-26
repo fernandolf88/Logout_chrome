263597d6cb3177ace77a3255d263a43e9cbfc44f
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-26 09:19:22 -0300

    Atualizando denovo achei que já tava pronto

--- finalizador.ps1
+++ finalizador.ps1
@@ -1,6 +1,7 @@
 # Remover todos os perfis do Chrome
 $userName = $env:USERNAME
 $chromeUserDataPath = "C:\Users\$userName\AppData\Local\Google\Chrome\User Data"
+$homePage = "https://www.colegioavicenna.com.br"
 
 # Obter lista de diretórios de perfil
 $profileDirectories = Get-ChildItem -Path $chromeUserDataPath 
@@ -10,8 +11,17 @@ foreach ($profileDirectory in $profileDirectories) {
     Remove-Item -Path $profileDirectory.FullName -Force -Recurse
 }
 
-New-Item -ItemType Directory -Path "$chromeUserDataPath\Default"
-New-Item -ItemType File -Path "$chromeUserDataPath\Default\Preferences" -Value '{"homepage": "https://www.google.com"}'
+$preferencesValue = '{"homepage": "'+$homePage+'", "startup_urls": ["'+ $homePage +'"]}'
+New-Item -ItemType File -Path "$chromeUserDataPath\Default\Preferences" -Value $preferencesValue
+
+$shortcutPath = "C:\Users\$userName\Desktop\Chrome Avicenna.lnk"
+$targetPath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$arguments = "--profile-directory=Default"
 
 Remove-Item -Path "C:\Users\$userName\Desktop\*Chrome*.lnk" -Force
 Write-Host "Script executado com exito."
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null
+$shortcut = New-Object -ComObject WScript.Shell
+$shortcut.CreateShortcut($shortcutPath).TargetPath = $targetPath
+$shortcut.CreateShortcut($shortcutPath).Arguments = $arguments
--- install_script.bat
+++ install_script.bat
@@ -1,10 +1,12 @@
 @ECHO ON
 
-set url=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/finalizador.ps1
+set url1=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/finalizador.ps1
+set url2=https://raw.githubusercontent.com/fernandolf88/Logout_chrome/main/executar.bat
 mkdir C:\script\
-set output = C:\script\logout.ps1
-curl %url% -o c:\script\logout.ps1
+curl %url1% -o c:\script\logout.ps1
+curl %url2% -o c:\script\executar.bat
 schtasks /create /tn "Logout Chrome" /tr "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File C:\script\logout.ps1" /sc onlogon
+schtasks /create /tn "Run Script" /tr "C:\executar.bat" /sc onlogon
 attrib +h c:\script
 schtasks /run /tn "Logout Chrome"
 msg * "Script instalado com sucesso!"
