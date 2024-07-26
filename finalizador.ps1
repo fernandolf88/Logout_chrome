d64a9e484cc93e11738ac320d633c48f85fbe67f
fernandolf88 <fernandolf@alumni.usp.br>
2024-07-26 12:43:21 -0300

    aaahhhh

--- finalizador.ps1
+++ finalizador.ps1
@@ -1,10 +1,11 @@
 # Remover todos os perfis do Chrome
 $userName = $env:USERNAME
 $chromeUserDataPath = "C:\Users\$userName\AppData\Local\Google\Chrome\User Data"
-$homePage = "https://www.colegioavicenna.com.br"
 
 # Obter lista de diretórios de perfil
 $profileDirectories = Get-ChildItem -Path $chromeUserDataPath 
+
+Get-Process -Verbose:$false
 
 # Remover cada diretório de perfil
 foreach ($profileDirectory in $profileDirectories) {
@@ -13,6 +14,6 @@ foreach ($profileDirectory in $profileDirectories) {
 
 Remove-Item -Path "C:\Users\$userName\Desktop\*.lnk" -Force
 
-Expand-Archive -Path "C:\script\User.zip" -DestinationPath "C:\Users\$userName\AppData\Local\Google\Chrome\"
+Expand-Archive -Path "C:\script\User.zip" -DestinationPath "C:\Users\$userName\AppData\Local\Google\Chrome\" -Force
 
 Write-Host "Script executado com exito."
